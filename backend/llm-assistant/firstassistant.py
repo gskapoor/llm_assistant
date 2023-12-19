d1135bbf2d8d3dbe11d3bf500c6957b78f9f7513
Alan Acer Laptop <abuckser@ucdavis.edu>
2023-12-18 22:11:31 -0800

    Assistant can hold conversation with static input. - Basic conversational functions as an "assistant":   - makes future wolfram integration etc. easier  - automated time gaps to allow LLM to finish answer  - next steps:    - end conversation    - correctly start/continue conversation with new input    - figure out where to store assistant and thread

--- backend/llm-assistant/firstassistant.py
+++ backend/llm-assistant/firstassistant.py
@@ -1,16 +1,79 @@
 from dotenv import load_dotenv
+import asyncio
+import time
 load_dotenv()
 
 
-from openai import OpenAI
-client = OpenAI()
+#from openai import OpenAI
+from openai import AsyncOpenAI
+client = AsyncOpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+async def initialize_conversation():
+  assistant = await client.beta.assistants.create(
+      name="Personal Assistant",
+      instructions="You are a personal assistant whose role is to help the user complete their tasks and to entertaing the user via conversation.",
+      model="gpt-3.5-turbo-1106"
+  )
 
-print(completion.choices[0].message)
+  thread = await client.beta.threads.create()
+
+  return (assistant, thread)
+
+
+
+async def continue_conversation(assistant, thread, next_message):
+
+  message = await client.beta.threads.messages.create(
+      thread_id=thread.id,
+      role="user",
+      content=next_message
+  )
+
+  run = await client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
+
+  while True:
+    run = await client.beta.threads.runs.retrieve(
+      thread_id=thread.id,
+      run_id=run.id
+    )
+
+    match run.status:
+      case "cancelled":
+        print("cancelled")
+        break
+      case "failed":
+        print("failed")
+        break
+      case "expired":
+        print("expired")
+        break
+      case "completed":
+        break
+      case _:
+        time.sleep(3) # Note that 3 is arbitrary
+
+  messages = await client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  
+  return messages.data[0].content[0].text.value
+
+
+
+async def start_conversation(initial_message):
+
+  (assistant, thread) = await initialize_conversation()
+
+  messages = await continue_conversation(assistant, thread, initial_message)
+
+  return (assistant, thread, messages)
+
+async def main() -> None:
+  (assistant, thread, my_conversation) = await start_conversation("Hi! What is 2 + 2?")
+  myer_conversation = await continue_conversation(assistant, thread, "What are the colors of the rainbow? Nothing else after that.")
+  print(myer_conversation)
+
+asyncio.run(main())
