bab8f0edba7374971aa48d0532e6e37cba9ad1f1
gskapoor <kapoorgobind@gmail.com>
2023-12-20 20:09:46 -0800

    made less verbose main

--- backend/go/main.go
+++ backend/go/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/gskapoor/llm_assistant/backend/go/handlers"
-	middleware "github.com/gskapoor/llm_assistant/backend/go/shared"
+	midw "github.com/gskapoor/llm_assistant/backend/go/shared"
 )
 
 func textHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,10 +18,11 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/text", middleware.Chain(handlers.HandleTextInput, middleware.LoggingStart(), middleware.Logging()))
-	r.HandleFunc("/voice", middleware.Chain(handlers.HandleVoiceInput, middleware.LoggingStart(), middleware.Logging()))
+	r.HandleFunc("/text", midw.Chain(handlers.HandleTextInput, midw.LoggingStart(), midw.Logging()))
+	r.HandleFunc("/voice", midw.Chain(handlers.HandleVoiceInput, midw.LoggingStart(), midw.Logging()))
 
 	fmt.Println("Server is running on :8080")
+
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		fmt.Println(err)
