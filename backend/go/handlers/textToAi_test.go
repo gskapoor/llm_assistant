d259c4aa0fd0c64538cd10290853ef1aa42aa9e7
gskapoor <kapoorgobind@gmail.com>
2023-12-24 12:07:39 -0800

    consolidated tests into one function

--- backend/go/handlers/textToAi_test.go
+++ backend/go/handlers/textToAi_test.go
@@ -9,32 +9,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAssistantInit_Success(t *testing.T) {
-	// Create a mock HTTP server
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, `{"assistant_session":{"assistant_id":"test_id","thread_id":"test_thread"}}`)
-	}))
-	defer server.Close()
+func TestAssistantInit(t *testing.T) {
+	t.Run("Test Success", func(t *testing.T) {
+		// Create a mock HTTP server
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `{"assistant_session":{"assistant_id":"test_id","thread_id":"test_thread"}}`)
+		}))
+		defer server.Close()
 
-	// Call the function being tested with the mock server URL
-	session, err := assistantInit(server.URL)
-	assert.NoError(t, err, "Expected no error")
+		// Call the function being tested with the mock server URL
+		session, err := assistantInit(server.URL)
+		assert.NoError(t, err, "Expected no error")
 
-	// Verify the result
-	assert.Equal(t, "test_id", session.AssistantSession.AssistantID, "Unexpected assistant ID")
-	assert.Equal(t, "test_thread", session.AssistantSession.ThreadID, "Unexpected thread ID")
-}
+		// Verify the result
+		assert.Equal(t, "test_id", session.AssistantID, "Unexpected assistant ID")
+		assert.Equal(t, "test_thread", session.ThreadID, "Unexpected thread ID")
+	})
 
-func TestAssistantInit_ErrorHTTP(t *testing.T) {
+	t.Run("HTTP Error", func(t *testing.T) {
+		// Create a mock HTTP server that returns an error status code
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}))
+		defer server.Close()
 
-	// Create a mock HTTP server that returns an error status code
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}))
-	defer server.Close()
+		session, err := assistantInit(server.URL)
 
-	session, err := assistantInit(server.URL)
+		assert.Error(t, err, "Expected an error")
+		assert.Equal(t, AssistantSession{}, session, "Expected an empty session on error")
+	})
 
-	assert.Error(t, err, "Expected an error")
-	assert.Equal(t, AssistantSession{}, session, "Expected an empty session on error")
 }
