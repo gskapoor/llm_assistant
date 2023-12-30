185bbd336000c49eb6cb414ef10cb450b1333f2b
knagashima721 <nagashimakyoka0326@gmail.com>
2023-12-29 23:26:07 -0800

    log error added, and changed tts.go

--- backend/go/handlers/textToSpeech.go
+++ backend/go/handlers/textToSpeech.go
@@ -2,77 +2,62 @@ package handlers
 
 import (
 	"bytes"
-	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-type TextToSpeechRequest struct {
-	Text string `json:"text"`
-}
-
 func TextToSpeechHandler(w http.ResponseWriter, r *http.Request) {
-
-	godotenv.Load(".env")
-
-	uberDuckAPIURL := os.Getenv("TTS_URL")
-	uberDuckAPIKey := os.Getenv("TTS_API_KEY")
-
+	openAIURL := os.Getenv("TTS_URL")
+	apiKey := os.Getenv("TTS_API_KEY")
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
-	// Read the request body containing the text to be converted to speech
-	body, err := io.ReadAll(r.Body)
+	// Define the input text for TTS
+	inputText := "Today is a wonderful day to build something people love!"
+
+	// Prepare the request body
+	requestBody := fmt.Sprintf(`{"model":"tts-1", "voice":"alloy", "input":"%s"}`, inputText)
+	// Create HTTP POST request to OpenAI TTS API
+	req, err := http.NewRequest("POST", openAIURL, bytes.NewBuffer([]byte(requestBody)))
 	if err != nil {
 		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		http.Error(w, "Error creating request", http.StatusInternalServerError)
+		return
 	}
 
-	// Create a request object for UberDuck's API
-	requestData := TextToSpeechRequest{Text: string(body)}
-	requestJSON, err := json.Marshal(requestData)
-	if err != nil {
-		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to create request JSON", http.StatusInternalServerError)
-	}
-
-	// Create an HTTP POST request to UberDuck's API
-	req, err := http.NewRequest(http.MethodPost, uberDuckAPIURL, bytes.NewBuffer(requestJSON))
-	if err != nil {
-		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to create HTTP request", http.StatusInternalServerError)
-	}
-	req.Header.Set("Authorization", "Bearer "+uberDuckAPIKey)
+	req.Header.Set("Authorization", "Bearer "+apiKey)
 	req.Header.Set("Content-Type", "application/json")
 
-	// Send the request to UberDuck's API
+	// Send request
 	client := &http.Client{}
-	response, err := client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to make request to UberDuck's API", http.StatusInternalServerError)
+		http.Error(w, "Error sending request", http.StatusInternalServerError)
+		return
 	}
-	defer response.Body.Close()
+	defer resp.Body.Close()
 
-	// Read the response from UberDuck's API
-	audioData, err := io.ReadAll(response.Body)
+	// Read response body (audio content)
+	audioContent, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to read response from UberDuck's API", http.StatusInternalServerError)
+		http.Error(w, "Error reading response", http.StatusInternalServerError)
+		return
 	}
 
-	// Set the Content-Type header for the audio response
-	w.Header().Set("Content-Type", "audio/mpeg") // Adjust the content type based on the actual response
-
-	// Write the audio data as the response
-	_, err = w.Write(audioData)
+	// Write audio content to file (speech.mp3)
+	err = os.WriteFile("speech.mp3", audioContent, 0644)
 	if err != nil {
 		log.Printf("HTTP error: %v", err)
-		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+		http.Error(w, "Error writing to file", http.StatusInternalServerError)
+		return
 	}
+
+	fmt.Fprintln(w, "Speech saved to speech.mp3")
 }
