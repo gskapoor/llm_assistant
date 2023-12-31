bde9c6018f2e89d19aaf122d2ca20f5f9d6d7942
vulcanz <abza@ucdavis.edu>
2023-12-30 22:15:55 -0800

    Fully working whitespace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,21 +33,17 @@ function App() {
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
 
-    genMessage({author: "user", text: formJson.message, audio: null})
-
-    //Made logic statements to handle whitespace, checked message after trim to see if there is a message after function
-    //else throws typical response.
-
     if(formJson.message.trim().length == 0) {
       const responseJson = {message: "Oops! Looks like you forgot to write something!"};
       genMessage({author: "maya", text: responseJson.message, audio: null})
+      formRef.current.reset();
     }
     else{
+      genMessage({author: "user", text: formJson.message, audio: null})
       const responseJson = {message: "[insert API response here]"};
       genMessage({author: "maya", text: responseJson.message, audio: null})
+      formRef.current.reset();
     }
-    
-
   }
   
   function handleEnter(e) {
